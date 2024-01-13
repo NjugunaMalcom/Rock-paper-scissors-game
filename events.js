1955c3f2cfd2082c6aa2801c3e3e09f5a94134ef
NjugunaMalcom <malcomsammy18@gmail.com>
2024-01-13 13:13:43 -0800

    almost towards playing music with a click on a button

--- events.js
+++ events.js
@@ -1,4 +1,4 @@
-
+// through the querySelector method we get the id's and through the event listeners we listen to mouse events then carry out the function involving the button clicked.
 const rockButtonElement = document.querySelector('#rock-button');
 
    rockButtonElement.addEventListener('click', () => {
@@ -16,4 +16,4 @@ const scissorButtonElement = document.querySelector('#scissors-button');
     scissorButtonElement.addEventListener('click', () => {
         gamePlayerMove('Scissors');
 
-    })    
+    })   
--- move.js
+++ move.js
@@ -1,9 +1,11 @@
-const score = {
-    Wins : 0,
-    Losses : 0,
-    Ties : 0,
+let score = JSON.parse(localStorage.getItem('score'));
+if (!score){
+  score = {Wins :0,
+           Losses :0,
+           Ties :0};
 }
 
+ // here the function compares the playermove to the computermove and updates the the score object and then display the result ,move comparison and scores.
 function gamePlayerMove(playerMove) {
 
     const computerMove = pickCompterMove();
@@ -35,22 +37,42 @@ function gamePlayerMove(playerMove) {
                 result = 'You Tie';
             }
         } 
+// using the if statement we compare results to update results according to wins ,losses and ties.
 
         if (result === 'You Win'){
             score.Wins +=1;
-1        }else if (result === 'You Lose'){
+         }else if (result === 'You Lose'){
             score.Losses +=1;
           }else if (result === 'You Tie'){
             score.Ties +=1;
           }
- alert(`You picked ${playerMove}. Computer picked ${computerMove}.${result}.
- Wins:${score.Wins},Losses:${score.Losses},Ties:${score.Ties}`); //displays the message in  a pop up
 
+          localStorage.setItem('score', JSON.stringify(score));
+
+          const resetButtonElement = document.querySelector('#Reset-scores');
+
+        resetButtonElement.addEventListener ('click', () =>{
+
+            score.Wins = 0;
+            score.Losses = 0;
+            score.Ties = 0
+        })
+
+// here the scores are updated to the  paragraph ,I used the queryselector to find the classes of the paragraph then through .innerHTML property to attach the result,move comparison and scores.
+
+            const resultElement = document.querySelector('.results'); 
+                resultElement.innerHTML =`${result}`;
+                
+            const moveComparisonElement = document.querySelector('.move-comparison');  
+                    moveComparisonElement.innerHTML =`You picked ${playerMove}.Computer picked ${computerMove}`; 
+
+            const scoreElement = document.querySelector('.scores');
+                    scoreElement.innerHTML =`Wins:${score.Wins},Losses:${score.Losses},Ties:${score.Ties}`;
 }
+// In this function below computer picks  a random number that is between 0 and 1 using the math.random() function then attaches it to a move according to the random number obtained and finally returns the move.
+function pickCompterMove(){  
 
-function pickCompterMove(){
-
-        let randomNumber = Math.random();
+         let randomNumber = Math.random();           
         let computerMove ='';
             if(randomNumber > 0 &&randomNumber <1/3){
             computerMove = 'Rock';
